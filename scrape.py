99e23c4c07a4e0bf70888a5945c5bc7572a05587
BrodyKeane <keanebrody8585@gmail.com>
2023-01-23 23:45:38 -0500

    expanded url permission checker

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import requests
 from robotexclusionrulesparser import RobotExclusionRulesParser
 from urllib.parse import urlparse
+import socket
 
 
 class WebScraper:
@@ -14,27 +15,58 @@ class WebScraper:
 class URLPermissionChecker:
     def __init__(self, url):
         self.url = url
+        self.parsed_url = urlparse(url)
 
     def can_scrape(self):
+        if not self._is_valid_url():
+            return False
         robot_parser = self._parse_robots_txt()
         can_scrape = robot_parser.is_allowed("User-agent", self.url)
         return can_scrape
 
-    def get_root_url(self):
-        parsed_url = urlparse(self.url)
-        root = parsed_url.scheme + "://" + parsed_url.netloc
-        return root
-    
-    def _get_robots_txt(self):
-        root_url = self.get_root_url()
-        robots_url = root_url + '/robots.txt'
-        response = requests.get(robots_url)
-        robots_txt = response.text
-        return robots_txt
+
+    def _is_valid_url(self):
+        is_valid = (
+            self._is_https() and
+            self._is_valid_format() and
+            self._is_accessible()
+        )
+        return is_valid
+
+    def _is_https(self):
+        return self.parsed_url.scheme == "https"
+
+    def _is_valid_format(self):
+        try:
+            hostname = self.parsed_url.hostname
+            socket.gethostbyname(hostname)
+            return True
+        except:
+            return False
+
+    def _is_accessible(self):
+        try:
+            res = requests.get(self.url)
+            return res.status_code == 200
+        except:
+            return False
+
 
     def _parse_robots_txt(self):
         robots_txt = self._get_robots_txt()
         robot_parser = RobotExclusionRulesParser()
         robot_parser.parse(robots_txt)
         return robot_parser
+
+    def _get_robots_txt(self):
+        root_url = self._get_root_url()
+        robots_url = root_url + '/robots.txt'
+        response = requests.get(robots_url)
+        robots_txt = response.text
+        return robots_txt
+
+    def _get_root_url(self):
+        root = self.parsed_url.scheme + "://" + self.parsed_url.netloc
+        return root
+    
 
--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 import unittest
 from scrape import URLPermissionChecker
 
-class TestScrape(unittest.TestCase):
+class TestURLPermissionChecker(unittest.TestCase):
 
-    def test_permission_to_scrape_allowed(self):
+    def test_permission_to_scrape_from_root(self):
         url = 'https://www.w3schools.com'
+        test_scrape = URLPermissionChecker(url)
+        permission = test_scrape.can_scrape()
+        self.assertEqual(permission, True) 
+
+    def test_permission_to_scrape_from_sub_dir(self):
+        url = 'https://www.w3schools.com/html/html_blocks.asp'
         test_scrape = URLPermissionChecker(url)
         permission = test_scrape.can_scrape()
         self.assertEqual(permission, True) 
@@ -16,37 +22,32 @@ class TestScrape(unittest.TestCase):
         self.assertEqual(permission, False)
 
     def test_permission_to_scrape_on_404(self):
-        url = 'https://www.imdb.com/fake-url'
+        url = 'https://www.w3schools.com/fakdafse-url'
         test_scrape = URLPermissionChecker(url)
         permission = test_scrape.can_scrape()
-        self.assertEqual(permission, True)
+        self.assertEqual(permission, False)
 
-    def test_get_root_url(self):
-        url = 'https://www.w3schools.com/html/default.asp'
+    def test_permission_to_scrape_from_blank_url(self):
+        url = ''
         test_scrape = URLPermissionChecker(url)
-        root = test_scrape.get_root_url()
-        self.assertEqual(root, 'https://www.w3schools.com')
+        permission = test_scrape.can_scrape()
+        self.assertEqual(permission, False)
 
-    def test_get_root_url_from_root(self):
-        url = 'https://www.w3schools.com'
+    def test_permission_to_scrape_from_http(self):
+        url = 'http://www.w3schools.com'
         test_scrape = URLPermissionChecker(url)
-        root = test_scrape.get_root_url()
-        self.assertEqual(root, 'https://www.w3schools.com')
+        permission = test_scrape.can_scrape()
+        self.assertEqual(permission, False)
 
-    def test_get_root_url_from_404(self):
-        url = 'https://www.w3schools.com/fake-urll'
+    def test_permission_to_scrape_from_unformatted_url(self):
+        url = 'w3schools.com'
         test_scrape = URLPermissionChecker(url)
-        root = test_scrape.get_root_url()
-        self.assertEqual(root, 'https://www.w3schools.com')
+        permission = test_scrape.can_scrape()
+        self.assertEqual(permission, False)
 
-    def test_get_root_url_from_url(self):
-        url = 'asdfasdfasdfxlf[feamfai9efa'
-        test_scrape = URLPermissionChecker(url)
-        root = test_scrape.get_root_url()
-        self.assertEqual(root, '://')
 
-    def test_get_list_of_elements(self):
-        pass
+
+
 
 
 if __name__ == '__main__':
