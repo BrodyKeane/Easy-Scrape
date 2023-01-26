a6eb2efc171039e51a5e0d3d83f8cc736911ce8b
BrodyKeane <keanebrody8585@gmail.com>
2023-01-25 22:50:39 -0500

    created basic datascaper

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, url_for, redirect, request
-
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
 app.static_url_path = '/static'
 app.static_folder = 'static'
 
--- scrape.py
+++ scrape.py
@@ -1,14 +1,52 @@
-import requests
+from requests import get
 from robotexclusionrulesparser import RobotExclusionRulesParser
 from urllib.parse import urlparse
-import socket
+from socket import gethostbyname
+from bs4 import BeautifulSoup
 
-
-class WebScraper:
-    def __init__(self, url):
+class CorrelatedDataScraper:
+    def __init__(self, url, container_tag, data_tags, row_limit=100):
         self.url = url
-        self.permission_checker = URLPermissionChecker(url)
+        self.container_tag = container_tag
+        self.data_tags = data_tags
+        self.row_limit = row_limit
 
+    def scrape(self):
+        containers = self._find_containers()
+        rows = self._get_rows(containers)
+        return rows
+
+    def _find_containers(self):
+        response = get(self.url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        containers = soup.select(self.container_tag, limit=self.row_limit)
+        return containers
+
+    def _get_rows(self, containers):
+        rows = []
+        for container in containers:
+            row = self._populate_row(container)
+            rows.append(row)
+        return rows
+
+    def _populate_row(self, container):
+        row = {}
+        for data_tag in self.data_tags:
+            try:
+                data = container.select_one(data_tag).text
+                row[data_tag] = data
+            except:
+                row[data_tag] = f'{data_tag} not found'
+        return row
+
+    def set_container_tag(self, tag):
+        self.container_tag = tag
+
+    def add_data_tag(self, tag):
+        self.data_tags.append(tag)
+
+    def remove_data_tag(self, tag):
+        self.data_tags.remove(tag)
 
 
 
@@ -39,14 +77,14 @@ class URLPermissionChecker:
     def _is_valid_format(self):
         try:
             hostname = self.parsed_url.hostname
-            socket.gethostbyname(hostname)
+            gethostbyname(hostname)
             return True
         except:
             return False
 
     def _is_accessible(self):
         try:
-            res = requests.get(self.url)
+            res = get(self.url)
             return res.status_code == 200
         except:
             return False
@@ -61,7 +99,7 @@ class URLPermissionChecker:
     def _get_robots_txt(self):
         root_url = self._get_root_url()
         robots_url = root_url + '/robots.txt'
-        response = requests.get(robots_url)
+        response = get(robots_url)
         robots_txt = response.text
         return robots_txt
 
